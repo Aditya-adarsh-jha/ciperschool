102c22a201f9e4039c05caba2c4b1e5e73bffeb5
Aditya-adarsh-jha <jha240701adarsh@gmail.com>
2022-06-26 18:23:02 +0530

    updated

---
+++ components/products.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    title: "Nord Stage 3 88 keys",
+    description: "E.M.L.A Music",
+    price: "$5,738",
+    img: "https://www.nordkeyboards.com/sites/default/files/styles/top_image/public/files/products/nord-stage-3/images/Nord-Stage-3-overview.jpg?itok=aCkzwkzQ",
+    quantity: 1,
+  },
+  {
+    id: 2,
+    title: "Nord Lead A1",
+    description: "Gear Garage",
+    price: "$1,700",
+    img: "https://m.media-amazon.com/images/I/61X+9Mmv99L._SX522_.jpg",
+    quantity: 1,
+  },
+  {
+    id: 3,
+    title: "Moog Subsequent 37",
+    description: "TURNLAB",
+    price: "$1,700",
+    img: "https://theinventory.in/wp-content/uploads/2020/06/download_2934_moog_subsequent_37_01.jpg",
+    quantity: 1,
+  },
+];
